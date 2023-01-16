14e15f99a2a1dcd1942aa349c2ddeb907c831e1d
James O'Reilly <jp.oreilly@hotmail.com>
2023-01-16 15:38:00 +0000

    Update clone_git_repo.sh

--- 01--Infrastructure/01-Environment/clone_git_repo.sh
+++ 01--Infrastructure/01-Environment/clone_git_repo.sh
@@ -6,17 +6,18 @@
 # copy files over to /etc/ansible/playbooks
 # tidy up directory..
 # dont forget to close and open VSC ..
-#
-# 13/01/2023
+# 
+# If you reboot the server, clone_workshop.service will reset the /etc/ansible
+# 16/01/2023
 # =========================================================================================
 
-#set the loads of variables
+# set variables
 remoteHost=github.com
 remoteUser=jporeilly
 localUser=installer
 remoteDir=Workshop--DC-7.3
 remoteRepo=https://$remoteHost/$remoteUser/$remoteDir
-localDirW=~/Workshop--DC-7.3
+localDirW=/home/installer/Workshop--DC-7.3
 ansPlaybooks=/etc/ansible/playbooks
 mod_01E=$localDirW/01--Infrastructure/01-Environment
 mod_01A=$localDirW/01--Infrastructure/02-Ansible
@@ -29,29 +30,26 @@ if [ -d "$localDirW" -a ! -h "$localDirW" ]
 then
     echo "Directory $localDirW exists .." 
     echo "Deleting $localDirW .."
-    sudo rm -rf $localDirW
+         rm -rf $localDirW
 else
     echo "Error: Directory $localDirW does not exists .."
 fi
     echo "Creating $localDirW directory .."
-    sudo mkdir $localDirW
-    sudo git clone $remoteRepo $localDirW
-    sudo chown -R $localUser $localDirW
+         mkdir $localDirW
+         git clone $remoteRepo $localDirW
+         chown -R $localUser $localDirW
     echo "Deleting $ansPlaybooks .."
-    sudo rm -rfv $ansPlaybooks/*
+         rm -rfv $ansPlaybooks/*
     echo "Copying over Module 01 - Infrastructure .."
-    sudo cp -rfp $mod_01E/*  $ansPlaybooks
-    sudo cp -rfp $mod_01A/*  $ansPlaybooks
+         cp -rfp $mod_01E/*  $ansPlaybooks
+         cp -rfp $mod_01A/*  $ansPlaybooks
     echo "Copying over Module 02 - Preflight .."
-    sudo cp -rfp $mod_02/*  $ansPlaybooks
+         cp -rfp $mod_02/*  $ansPlaybooks
     echo "Copying over Module 03 - Data Catalog .."
-    sudo cp -rfp $mod_03/*  $ansPlaybooks
+         cp -rfp $mod_03/*  $ansPlaybooks
     echo "Copying over Module 04 - Workshop .."
-    sudo cp -rfp $mod_04/*  $ansPlaybooks
+         cp -rfp $mod_04/*  $ansPlaybooks
     echo "Copy over ansible configuration files .."
-    sudo cp -rfp 
     echo "Tidying up directory .."
-    sudo rm -rfv $ansPlaybooks/*.md
-    sudo rm -rfv $ansPlaybooks/resources
-    sudo rm -rfv $ansPlaybooks/assets
+         rm -rfv $ansPlaybooks/*.md
     echo "Latest Workshop--Data-Catalog copied over .. close and open VSC .."
